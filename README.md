262ac4f73c9b9d9bb31ab6b681a9f9a3fc04ee14
tikoot <tiko.otarashvili99@gmail.com>
2024-05-21 13:27:16 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Set up the project
+```
+git clone git@github.com:pllague/tbc-react-accelerartor.git
+```
+```
+cd tbc-react-accelerartor
+```
+```
+npm install
+```
+```
+npm run dev
+```
+If port 3000 is not busy you can open the project in your browser using the following [next-app](http://localhost:3000/) link
+
+## Happy Coding !!!
---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import createNextIntlPlugin from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        port: "",
+        pathname: "/product-images/**",
+      },
+    ],
+  },
+};
+
+// export { nextConfig };
+export default withNextIntl(nextConfig);
